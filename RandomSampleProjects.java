06578a76f1fcfab3d902ccfe8355cf771f0378c5
Hidayah Dabiri <monisoladab@yahoo.com>
2021-01-04 21:50:03 -0500

    Random Day 1

--- RandomSampleProjects.java
+++ RandomSampleProjects.java
@@ -4,7 +4,7 @@ public class RandomSampleProjects {
 
 	public static void main(String[] args) {
 
-		System.out.println("This program will multiply two numbers and display the result");
+		System.out.println("This program will compute two numbers and display the result");
 		System.out.println("=============================================================");
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Enter the first number: ");
@@ -14,6 +14,14 @@ public class RandomSampleProjects {
 		
 		int result = fnumber * snumber;
 		System.out.println("The result of "+fnumber+" * "+snumber+" is "+result);
+		
+		System.out.println(fnumber+ " + " +snumber +" = "+ (fnumber+snumber));
+		System.out.println(fnumber+ " - " +snumber +" = "+ (fnumber-snumber));
+		System.out.println(fnumber+ " / " +snumber +" = "+ (fnumber/snumber));
+		System.out.println(fnumber+ " mod " +snumber +" = "+ (fnumber%snumber));
+		System.out.println(fnumber+ " * " +snumber +" = "+ (fnumber*snumber));
+
+		
 		keyboard.close();
 		
 	}
