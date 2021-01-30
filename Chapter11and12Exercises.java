2b3aac0184e200254660bb55c3f515dc63a0316c
Hidayah Dabiri <monisoladab@yahoo.com>
2021-01-29 23:52:09 -0500

    completed programs in both chapters

--- Chapter11and12Exercises.java
+++ Chapter11and12Exercises.java
@@ -1,8 +1,7 @@
-import java.awt.print.Printable;
+
 import java.util.Random;
 import java.util.Scanner;
 
-import jdk.jshell.SourceCodeAnalysis.Completeness;
 
 public class Chapter11and12Exercises {
 
@@ -12,7 +11,11 @@ public class Chapter11and12Exercises {
 //		daysOfTheWeek();  
 //		timeToEat();
 //		tinyCalculator();
-		randomConditionalExercise();
+//		randomConditionalExercise();
+//		livingLarge();
+//		areWeThereYet();
+//		tallyUp();
+		twoInARow();
 	}
 
 	public static void daysOfTheWeek() {
@@ -130,20 +133,79 @@ public class Chapter11and12Exercises {
 	public static void randomConditionalExercise() {
 		
 		//Random randomNum = new Random();
-		int randomFixed = 5;
+		Random randomNum = new Random();
 		System.out.println("Please enter a random integer between 0 and 9: ");
 		Scanner keyboard = new Scanner(System.in);
 		int num1 = keyboard.nextInt();
-		int yes;
-		if (randomFixed == num1) {
+		int genRandom = randomNum.nextInt(10) + 1;
+	
+		if (genRandom == num1) {
 			System.out.println("You win!");
-			
 		}else {
 			System.out.println("You Lose!");
 		}
-		//unable to complete the examples on page 264 using the conditional statement.
-		//yes = (num1 == 5) ? System.out.println("You Win!") : System.out.println("You Lose!");
 		keyboard.close();
 	}
 	
+	public static void livingLarge() {
+		
+		Scanner keyboard = new Scanner(System.in);
+		int number;
+		
+		number = keyboard.nextInt();
+		while (number <= 100) {
+			number=keyboard.nextInt();
+		}
+		keyboard.close();
+	}
+	
+	public static void areWeThereYet() {
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please respond to the question with 'Y' or 'y' if we are there.");
+		System.out.println("Or respond with 'N' or 'n' if we are not there yet.");
+		char response = keyboard.findWithinHorizon(".", 0).charAt(0);
+		
+		while (response != 'y' && response != 'Y') { //I used || here and it continued to loop.
+			System.out.println("Are we there yet?");
+			response = keyboard.findWithinHorizon(".", 0).charAt(0);
+		}
+		System.out.println("Finally! We have arrived. Whew!");
+		keyboard.close();
+	}
+	
+	public static void tallyUp() {
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please enter any number and this program will add them all up for you");
+		System.out.println("If you enter a negative number, the total will be displayed without the negative number");
+		int input, total = 0;
+		input = keyboard.nextInt();
+		
+		while (input >= 0) {
+			System.out.println("You entered " + input);
+			total += input;
+			input = keyboard.nextInt();
+		}
+		System.out.println( "You total is " +total+ ".");
+		keyboard.close();
+	}
+	
+	public static void twoInARow() {
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please enter any positive number.");
+		System.out.println("The program will stop if you enter the same number twice back to back.");
+		int input1, input2;
+		input1 = keyboard.nextInt();
+		input2 = keyboard.nextInt();
+		
+		while (input1 != input2) {
+			
+			input1 = input2;
+			input2 = keyboard.nextInt();
+		}
+		System.out.println( "Yaay!");
+		keyboard.close();
+	}
 }
