e718152096b9d2675e4d3c9024acd364cc0517c2
Hidayah Dabiri <monisoladab@yahoo.com>
2021-02-26 22:34:24 -0500

    BMI Calculation exercise

--- Chapter17Exercises.java
+++ Chapter17Exercises.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
 public class Chapter17Exercises {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Person hidi = new Person();
+		double bmi;
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Please enter the height: ");
+		hidi.height = keyboard.nextDouble();
+		System.out.println("Please enter the weight: ");
+		hidi.weight = keyboard.nextDouble();
+		
+		bmi = hidi.weight / (hidi.height*hidi.height);
+		
+		System.out.println("The Body Mass Index is: "+ bmi);
+		
+		
 	}
 
 }
