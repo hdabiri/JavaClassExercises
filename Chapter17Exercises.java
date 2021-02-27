6ac2f83843e5c818bd16b46c80e2be435b25e511
Hidayah Dabiri <monisoladab@yahoo.com>
2021-02-26 22:46:31 -0500

    Three person activity

--- Chapter17Exercises.java
+++ Chapter17Exercises.java
@@ -4,7 +4,7 @@ public class Chapter17Exercises {
 
 	public static void main(String[] args) {
 
-		person1();
+		//person1();
 		threePeople();
 	}
 
@@ -22,24 +22,51 @@ public class Chapter17Exercises {
 		bmi = hidi.weight / (hidi.height*hidi.height);
 		
 		System.out.println("The Body Mass Index is: "+ bmi);
-		
+		System.out.println();
 		keyboard.close();
 	}
 	
 	public static void threePeople() {
 		
 		Person person01 = new Person();
-		double bmi;
+		Person person02 = new Person();
+		Person person03 = new Person();
+		
+		double bmi1, bmi2, bmi3;
 		Scanner keyboard = new Scanner(System.in);
 		
-		System.out.println("Please enter the height: ");
+		// for Person 1
+		System.out.println("Please enter the height of Person 1: ");
 		person01.height = keyboard.nextDouble();
-		System.out.println("Please enter the weight: ");
+		System.out.println("Please enter the weight of Person 1: ");
 		person01.weight = keyboard.nextDouble();
 		
-		bmi = person01.weight / (person01.height*person01.height);
+		bmi1 = person01.weight / (person01.height*person01.height);
 		
-		System.out.println("The Body Mass Index is: "+ bmi);
+		System.out.println("The Body Mass Index for Person 1 is: "+ bmi1);
+		System.out.println("=========================");
+		
+		// For Person 2
+		System.out.println("Please enter the height of Person 2: ");
+		person02.height = keyboard.nextDouble();
+		System.out.println("Please enter the weight of Person 2: ");
+		person02.weight = keyboard.nextDouble();
+		
+		bmi2 = person02.weight / (person02.height*person02.height);
+		
+		System.out.println("The Body Mass Index for Person 2 is: "+ bmi2);
+		System.out.println("=========================");
+		
+		// For Person 3
+		System.out.println("Please enter the height of Person 3: ");
+		person03.height = keyboard.nextDouble();
+		System.out.println("Please enter the weight of Person 3: ");
+		person03.weight = keyboard.nextDouble();
+		
+		bmi3 = person03.weight / (person03.height*person03.height);
+		
+		System.out.println("The Body Mass Index for Person 3 is: "+ bmi3);
+		System.out.println("=========================");
 		
 		keyboard.close();
 		
