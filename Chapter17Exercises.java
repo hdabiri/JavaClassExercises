9a2cc36f674edb4e4987b95fb903b81bc6db83f2
Hidayah Dabiri <monisoladab@yahoo.com>
2021-02-26 22:37:49 -0500

    git

--- Chapter17Exercises.java
+++ Chapter17Exercises.java
@@ -4,6 +4,12 @@ public class Chapter17Exercises {
 
 	public static void main(String[] args) {
 
+		person1();
+		threePeople();
+	}
+
+	public static void person1() {
+		
 		Person hidi = new Person();
 		double bmi;
 		Scanner keyboard = new Scanner(System.in);
@@ -17,7 +23,26 @@ public class Chapter17Exercises {
 		
 		System.out.println("The Body Mass Index is: "+ bmi);
 		
+		keyboard.close();
+	}
+	
+	public static void threePeople() {
+		
+		Person person01 = new Person();
+		double bmi;
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Please enter the height: ");
+		person01.height = keyboard.nextDouble();
+		System.out.println("Please enter the weight: ");
+		person01.weight = keyboard.nextDouble();
+		
+		bmi = person01.weight / (person01.height*person01.height);
+		
+		System.out.println("The Body Mass Index is: "+ bmi);
+		
+		keyboard.close();
 		
 	}
-
 }
+
