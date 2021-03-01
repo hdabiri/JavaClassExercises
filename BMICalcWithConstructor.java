146daae411868ede49cf8bc14977a2d4bacad55f
Hidayah Dabiri <monisoladab@yahoo.com>
2021-03-01 18:04:11 -0500

    Created a new BMICalcWithConstructor to practice

---
+++ BMICalcWithConstructor.java
@@ -0,0 +1,13 @@
+
+public class BMICalcWithConstructor {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		
+		Person tope = new Person(65, 1.6);
+		Person jide = new Person(80, 1.75);
+		Person lara = new Person(85, 1.4);
+		
+	}
+
+}
--- Person.java
+++ Person.java
@@ -3,5 +3,18 @@ public class Person {
 
 	double weight;
 	double height;
+		
+	//Generated Constructor
+	public Person(double weight, double height) {
+		super();
+		this.weight = weight;
+		this.height = height;
+			
+		double bmi = weight / (height*height);
+		
+		System.out.println("The Body Mass Index is: "+ bmi);
+		System.out.println();
+	}
+
 	
 }
