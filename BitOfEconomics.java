77ed2c710d9b242544204a7022e2b005a0d86e14
Hidayah Dabiri <monisoladab@yahoo.com>
2021-03-01 21:27:04 -0500

    Bit of Economics and Country Class

---
+++ BitOfEconomics.java
@@ -0,0 +1,13 @@
+
+public class BitOfEconomics {
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		Country nigeria = new Country(14.7, 5.0);
+		Country uganda = new Country(15.0, 12.0);
+		Country rwanda = new Country(14.3, 18.5);
+		
+
+	}
+
+}
---
+++ Country.java
@@ -0,0 +1,24 @@
+
+public class Country {
+
+	double debt;
+	double gdp;
+	
+		//Constructor
+		public Country (double debt, double gdp) {
+		super();
+		this.debt = debt;
+		this.gdp = gdp;
+		
+		double ratio = debt/gdp;
+		System.out.println("The debt-to-GDP ratio is: " + ratio);
+		
+		if (ratio <= 0.8) {
+			System.out.println("This is an acceptable ratio. Yaay!!");
+			System.out.println();
+		} else {
+			System.out.println("This is not an acceptable ratio. :( ");
+			System.out.println();
+		}
+		}
+}
