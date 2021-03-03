668fe9bcf7301192b796d162bdd115f6f5cb0807
Hidayah Dabiri <monisoladab@yahoo.com>
2021-03-02 20:37:25 -0500

    BitOfEconomics Updated with Country name displayed in output

--- Country.java
+++ Country.java
@@ -3,15 +3,17 @@ public class Country {
 
 	double debt;
 	double gdp;
+	String name;
 	
 		//Constructor
-		public Country (double debt, double gdp) {
+		public Country (double debt, double gdp, String name) {
 		super();
 		this.debt = debt;
 		this.gdp = gdp;
+		this.name = name;
 		
 		double ratio = debt/gdp;
-		System.out.println("The debt-to-GDP ratio for " +this+ " is: " + ratio);
+		System.out.println("The debt-to-GDP ratio for "+ name + " is: " + ratio);
 		
 		if (ratio <= 0.8) {
 			System.out.println("This is an acceptable ratio. Yaay!!");
@@ -20,5 +22,13 @@ public class Country {
 			System.out.println("This is not an acceptable ratio. :( ");
 			System.out.println();
 		}
+	}
+		@Override
+		public String toString() {
+			return name;
 		}
+
+	
+
+		
 }
