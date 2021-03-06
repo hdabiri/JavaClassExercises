0e9f7379d3832997312bb3d4af48b597b0b805e8
Hidayah Dabiri <monisoladab@yahoo.com>
2021-03-05 21:45:06 -0500

    edited

--- MemorizationAppCoded.java
+++ MemorizationAppCoded.java
@@ -11,14 +11,7 @@ public class MemorizationAppCoded {
 		MemorizationApp hidi2 = new MemorizationApp("Hidayah2", x);
 
 
-		
-		//		I noticed that when i try to run both in the same run, it gives errors as follows: 
-		//		'Exception in thread "main"' in the output of the first object, and 
-		//		'java.util.NoSuchElementException: No line found
-		//		at java.base/java.util.Scanner.nextLine(Scanner.java:1651)
-		//		at MemorizationApp.<init>(MemorizationApp.java:19)
-		//		at MemorizationAppCoded.main(MemorizationAppCoded.java:13)' instead of the output of the second object.
-		//		Individually however, they both run perfectly.
+	
 	}
 
 }
